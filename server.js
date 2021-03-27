8edb40c313c16fa32b7d10194285feed14d126b8
md2eoseo <md2eoseo@gmail.com>
2021-03-27 12:20:03 +0900

    bebel-node setup

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require("apollo-server");
+import { ApolloServer, gql } from "apollo-server";
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
