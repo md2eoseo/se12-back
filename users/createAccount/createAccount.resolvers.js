50aad6cafc6283add2c113276155374b71bd7739
md2eoseo <md2eoseo@gmail.com>
2021-04-08 13:25:16 +0900

    bcrypt 모듈로 password 암호화

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import client from '../../client';
 
 export default {
@@ -15,12 +16,14 @@ export default {
             error: '사용 중인 이메일입니다.',
           };
         }
+        // password 암호화
+        const hashedPassword = await bcrypt.hash(password, 10);
         // 존재하지 않는 email이라면, User 생성
         await client.user.create({
           data: {
             name,
             email,
-            password,
+            password: hashedPassword,
             address,
           },
         });
--- users/login/login.resolvers.js
+++ users/login/login.resolvers.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import client from '../../client';
 
@@ -12,7 +13,7 @@ export default {
           return { ok: false, error: '가입되지 않은 이메일입니다.' };
         }
         // 검색되는 유저 있으면, 비밀번호 비교
-        const passwordMatch = password === user.password;
+        const passwordMatch = await bcrypt.compare(password, user.password);
         // 비밀번호가 일치하지 않으면, 에러 발생
         if (!passwordMatch) {
           return { ok: false, error: '비밀번호가 일치하지 않습니다.' };
