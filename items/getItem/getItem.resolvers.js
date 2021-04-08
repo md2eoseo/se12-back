9fef99e8f6de54a838b9723ef0e2f637355db416
LimitofIntegral <ufshg@naver.com>
2021-04-08 16:55:35 +0900

    getItem 구현

--- items/getItem/getItem.resolvers.js
+++ items/getItem/getItem.resolvers.js
@@ -1,16 +1,42 @@
+import { Prisma } from '.prisma/client';
+import { transformDocument } from '@prisma/client/runtime';
+import { introspectionFromSchema } from 'graphql';
 import client from '../../client';
 
 export default {
   Query: {
     getItem_title: async (_, { title }) => {
       try {
-        const item = await client.item.findMany({ where: { title } });
-        if (!title) {
-          return { ok: false, error: '해당하는 책이 없습니다.' };
+        const item = await client.item.findMany({
+          where: {
+            title,
+            activate: true,
+          },
+        });
+        if (item != '') {
+          return { ok: true, item };
+        } else {
+          return { ok: false, error: '찾고자 하는 책이 없습니다.' };
         }
-        return { ok: true, item };
       } catch (error) {
-        return { ok: true, error };
+        return { ok: false, error };
+      }
+    },
+    getItem_author: async (_, { author }) => {
+      try {
+        const item = await client.item.findMany({
+          where: {
+            author,
+            activate: true,
+          },
+        });
+        if (item != '') {
+          return { ok: true, item };
+        } else {
+          return { ok: false, error: '찾고자 하는 책이 없습니다.' };
+        }
+      } catch (error) {
+        return { ok: false, error };
       }
     },
   },
--- items/getItem/getItem.typeDefs.js
+++ items/getItem/getItem.typeDefs.js
@@ -1,12 +1,13 @@
 import { gql } from 'apollo-server';
 
 export default gql`
-    type GetItemResponse {
-        ok: Boolean!
-        error: String
-        item: Item
-    }
-    type Query {
-        getItem_title(title: String!): GetItemResponse!
-    }
+  type GetItemResponse {
+    ok: Boolean!
+    error: String
+    item: Item
+  }
+  type Query {
+    getItem_title(title: String!): GetItemResponse!
+    getItem_author(author: String!): GetItemResponse!
+  }
 `;
