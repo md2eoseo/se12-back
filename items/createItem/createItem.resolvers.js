bfca99a09aa7c12ded72f3c25ca357c21c33cc29
LimitofIntegral <ufshg@naver.com>
2021-04-10 22:01:53 +0900

    try-catch 구문 적용

--- items/createItem/createItem.resolvers.js
+++ items/createItem/createItem.resolvers.js
@@ -3,20 +3,35 @@ import client from '../../client';
 export default {
   Mutation: {
     createItem: async (_, { title, author, price, contents, category, publisher, isbn, pressDate, activate }) => {
-      await client.item.create({
-        data: {
-          title,
-          author,
-          price,
-          contents,
-          category,
-          publisher,
-          isbn,
-          pressDate,
-          activate,
-        },
-      });
-      return { ok: true };
+      try {
+        const exist = await client.item.findFirst({
+          where: { isbn },
+        });
+
+        if (exist) {
+          return {
+            ok: false,
+            error: '존재하는 isbn 입니다.',
+          };
+        }
+
+        await client.item.create({
+          data: {
+            title,
+            author,
+            price,
+            contents,
+            category,
+            publisher,
+            isbn,
+            pressDate,
+            activate,
+          },
+        });
+        return { ok: true };
+      } catch (error) {
+        return { ok: false, error };
+      }
     },
   },
 };
