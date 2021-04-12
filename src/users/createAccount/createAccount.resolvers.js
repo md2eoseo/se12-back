b1bb60d9ef28ca8f9b495b61e2f80c4a492ab64a
md2eoseo <md2eoseo@gmail.com>
2021-04-12 16:54:30 +0900

    createAccount input에 userId 추가

--- src/users/createAccount/createAccount.resolvers.js
+++ src/users/createAccount/createAccount.resolvers.js
@@ -3,17 +3,17 @@ import client from '../../client';
 
 export default {
   Mutation: {
-    createAccount: async (_, { name, email, password, address }) => {
+    createAccount: async (_, { userId, password, email, name, address, role }) => {
       try {
         // 존재하는 email인지 확인
         const exist = await client.user.findFirst({
-          where: { email },
+          where: { OR: [{ userId }, { email }] },
         });
         // 존재하는 email이라면, 에러 발생
         if (exist) {
           return {
             ok: false,
-            error: '사용 중인 이메일입니다.',
+            error: '사용 중인 아이디/이메일입니다.',
           };
         }
         // password 암호화
@@ -21,10 +21,12 @@ export default {
         // 존재하지 않는 email이라면, User 생성
         await client.user.create({
           data: {
-            name,
-            email,
+            userId,
             password: hashedPassword,
+            email,
+            name,
             address,
+            role,
           },
         });
         return { ok: true };
--- src/users/users.typeDefs.js
+++ src/users/users.typeDefs.js
@@ -3,11 +3,18 @@ import { gql } from 'apollo-server';
 export default gql`
   type User {
     id: Int!
-    name: String!
-    email: String!
+    role: Role!
+    userId: String!
     password: String!
+    email: String!
+    name: String!
     address: String
     createdAt: String!
     updatedAt: String!
+  }
+
+  enum Role {
+    ADMIN
+    CUSTOMER
   }
 `;
