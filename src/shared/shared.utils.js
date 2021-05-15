70bc8e2274e7a35aa830da9b0b790a4a65a9fbad
md2eoseo <md2eoseo@gmail.com>
2021-05-15 16:43:36 +0900

    상품 이미지 업로드용 s3 버킷 연동

---
+++ src/shared/shared.utils.js
@@ -0,0 +1,23 @@
+import AWS from 'aws-sdk';
+
+AWS.config.update({
+  credentials: {
+    accessKeyId: process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+export const uploadToS3 = async (file, userId, folder) => {
+  const { createReadStream } = await file;
+  const readStream = createReadStream();
+  const objectName = `${folder}/${userId}-${Date.now()}`;
+  const { Location } = await new AWS.S3()
+    .upload({
+      Bucket: 'se12-bucket',
+      Key: objectName,
+      ACL: 'public-read',
+      Body: readStream,
+    })
+    .promise();
+  return Location;
+};
