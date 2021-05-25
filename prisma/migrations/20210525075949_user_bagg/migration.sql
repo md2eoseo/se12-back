fc22cd514ea8da7ae29daefa677ba78580d466a2
md2eoseo <md2eoseo@gmail.com>
2021-05-25 17:00:23 +0900

    User bag 필드 추가

---
+++ prisma/migrations/20210525075949_user_bagg/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Item" ADD COLUMN     "userId" INTEGER;
+
+-- AddForeignKey
+ALTER TABLE "Item" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
