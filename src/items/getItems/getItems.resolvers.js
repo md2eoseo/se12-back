7e1cc10dbf1ae0d8dfe723dececb5dc560525eb1
md2eoseo <md2eoseo@gmail.com>
2021-05-25 14:49:35 +0900

    관리자 상품 관리 Query lastId 필드 삭제

--- src/items/getItems/getItems.resolvers.js
+++ src/items/getItems/getItems.resolvers.js
@@ -2,8 +2,7 @@ import { Role } from '.prisma/client';
 import client from '../../client';
 import { protectedResolver } from '../../users/users.utils';
 
-const PAGE_SIZE = 20;
-const resolverFn = async (_, { term, sortMethod, minPrice, maxPrice, lastId }, { loggedInUser }) => {
+const resolverFn = async (_, { term, sortMethod, minPrice, maxPrice }, { loggedInUser }) => {
   try {
     if (loggedInUser.role !== Role.ADMIN) {
       return { ok: false, error: '관리자만 접근할 수 있습니다.' };
@@ -14,15 +13,8 @@ const resolverFn = async (_, { term, sortMethod, minPrice, maxPrice, lastId }, {
       },
       // TODO: 'PRICE_LOW' => SortMethod.PRICE_LOW
       orderBy: [...(sortMethod ? [{ price: sortMethod === 'PRICE_LOW' ? 'asc' : 'desc' }] : []), { createdAt: 'desc' }],
-      take: PAGE_SIZE,
-      skip: lastId ? 1 : 0,
-      ...(lastId && { cursor: { id: lastId } }),
     });
-    let lastItemId = null;
-    if (items.length) {
-      lastItemId = items[items.length - 1].id;
-    }
-    return { ok: true, items, lastId: lastItemId };
+    return { ok: true, items };
   } catch (error) {
     return { ok: false, error };
   }
--- src/items/getItems/getItems.typeDefs.js
+++ src/items/getItems/getItems.typeDefs.js
@@ -5,9 +5,8 @@ export default gql`
     ok: Boolean!
     error: String
     items: [Item]
-    lastId: Int
   }
   type Query {
-    getItems(term: String, sortMethod: SortMethod, minPrice: Int, maxPrice: Int, lastId: Int): getItemsResponse!
+    getItems(term: String, sortMethod: SortMethod, minPrice: Int, maxPrice: Int): getItemsResponse!
   }
 `;
