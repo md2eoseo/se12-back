10d2afbb9207a8f1e270c9ac03ffba5bf1df4812
md2eoseo <md2eoseo@gmail.com>
2021-05-25 16:11:39 +0900

    searchItems lastId 제거

--- src/items/searchItems/searchItems.resolvers.js
+++ src/items/searchItems/searchItems.resolvers.js
@@ -1,9 +1,8 @@
 import client from '../../client';
 
-const PAGE_SIZE = 20;
 export default {
   Query: {
-    searchItems: async (_, { term, categoryId, minPrice, maxPrice, lastId }) => {
+    searchItems: async (_, { term, categoryId, minPrice, maxPrice }) => {
       try {
         const items = await client.item.findMany({
           where: {
@@ -16,15 +15,8 @@ export default {
             ],
           },
           orderBy: { createdAt: 'desc' },
-          take: PAGE_SIZE,
-          skip: lastId ? 1 : 0,
-          ...(lastId && { cursor: { id: lastId } }),
         });
-        let lastItemId = null;
-        if (items) {
-          lastItemId = items[items.length - 1].id;
-        }
-        return { ok: true, items, lastId: lastItemId };
+        return { ok: true, items };
       } catch (error) {
         return { ok: false, error };
       }
--- src/items/searchItems/searchItems.typeDefs.js
+++ src/items/searchItems/searchItems.typeDefs.js
@@ -5,9 +5,8 @@ export default gql`
     ok: Boolean!
     error: String
     items: [Item]
-    lastId: Int
   }
   type Query {
-    searchItems(term: String, categoryId: Int, minPrice: Int, maxPrice: Int, lastId: Int): searchItemsResponse!
+    searchItems(term: String, categoryId: Int, minPrice: Int, maxPrice: Int): searchItemsResponse!
   }
 `;
